94cc8b46ecfd65382a4a24030ff2503c9df6cdb7
Mark Grothe <grothem@gmail.com>
2023-03-30 22:36:01 -0500

    scaffolding out project

---
+++ src/pages/bios.tsx
@@ -0,0 +1,14 @@
+import { Footer } from "@/components/Footer";
+import Header from "@/components/Header";
+
+export default function Bios() {
+  return (
+    <>
+      <Header />
+      <div>
+        <h1>Bios</h1>
+      </div>
+      <Footer />
+    </>
+  );
+}
