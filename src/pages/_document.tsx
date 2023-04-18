d6629c0d5898fc4c2a1d8a671ec4f37c0d3b5e9e
Mark Grothe <grothem@gmail.com>
2023-04-17 22:24:47 -0500

    adjust theme color

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <meta name="theme-color" content="#CCCCCC" />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
